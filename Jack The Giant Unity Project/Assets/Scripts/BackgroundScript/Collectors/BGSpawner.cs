b5e189bdd4b78a1f03ec8fbba2f899fc8802290b
david <davss0802@gmail.com>
2020-05-07 18:56:16 +0400

    Infinite Background

---
+++ Jack The Giant Unity Project/Assets/Scripts/BackgroundScript/Collectors/BGSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BGSpawner : MonoBehaviour
+
+{
+    private GameObject[] backgrounds;
+    private float lastY;
+
+    void Start()
+    {
+
+        GetBackgroundsAndSetLastY();
+
+    }
+
+    void GetBackgroundsAndSetLastY()
+    {
+        backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        lastY = backgrounds[0].transform.position.y;
+
+        for (int i = 1; i < backgrounds.Length; i++)
+        {
+            if (lastY > backgrounds[i].transform.position.y)
+            {
+                lastY = backgrounds[i].transform.position.y;
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Background")
+        {
+            if (collision.transform.position.y == lastY)
+            {
+                Vector3 temp = collision.transform.position;
+                float height = ((BoxCollider2D)collision).size.y;
+
+                for (int i = 0; i < backgrounds.Length; i++)
+                {
+                    if (!backgrounds[i].activeInHierarchy)
+                    {
+                        temp.y -= height;
+                        lastY = temp.y;
+                        backgrounds[i].transform.position = temp;
+                        backgrounds[i].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+
+}
