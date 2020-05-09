91374f0726e19f24fdcbdb55609388c56a04aa2e
david <davss0802@gmail.com>
2020-05-09 15:27:55 +0400

    Navigating between Scenes

---
+++ Jack The Giant Unity Project/Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartGame(){
+         SceneManager.LoadScene("GamePlay");
+    }
+
+    public void HighScoreMenu(){
+        SceneManager.LoadScene("HighScoreScene");
+    }
+
+    public void OptionsMenu(){
+        SceneManager.LoadScene("OptionsMenuScene");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void MusicButton(){
+        
+    }
+   
+}
